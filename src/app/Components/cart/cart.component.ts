bf426a5c5e337328689870d5f912ed8a5dc38aee
EhabAhmed <e.ahmed2684@gmail.com>
2025-05-16 18:11:00 +0300

    Divide Cart Css into two Files

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -13,103 +13,98 @@ import { CartWishingDataService } from '../../Service/cart-wishing-data.service'
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css',
-  imports:[CommonModule , FormsModule ,RouterModule]
+  styleUrls: ['./cart.component.css', './cart.component2.css'],
+  imports: [CommonModule, FormsModule, RouterModule]
 })
 export class CartComponent implements OnInit {
-     Cart: CustomerBasket ={
-       id: "",
-       items: []
-     }
-    Shipping : shipping[] ;
-    SelectValue: number=0;
-    Subtotal:number = 0;
-    Total : number = 0;
-    EmptyCart:boolean =true;
+  Cart: CustomerBasket = {
+    id: "",
+    items: []
+  }
+  Shipping: shipping[];
+  SelectValue: number = 0;
+  Subtotal: number = 0;
+  Total: number = 0;
+  EmptyCart: boolean = true;
 
-   
-    
 
-    constructor ( private cartService : CartService ,private signal : CartWishingDataService , Shipping : EgyptShippingService , private sharedService : SharedServiceService)
-    {
-      this.Shipping = Shipping.shipping ;
-  
-    }
 
-    ngOnInit(): void {
-      
-        this.cartService.getCart().subscribe({next :(data) => {this.Cart = data,
-          this.Cart.items.forEach(x=> {
-            this.Subtotal += x.price * x.quantity;
-           });
-           this.sharedService.SubTotal.next(this.Subtotal);
-           if(this.Cart.items.length == 0)
-             this.EmptyCart= false;
-         }, 
-        error :(error)=>this.EmptyCart=false
-      });
-    
-       this.Total = this.Subtotal;
-      console.log(this.Cart);
-      //this.signal.cartItemsCount.set(this.Cart.items.length);
 
-      
-      
-    }
-   UpdateTotals()
-   {
-     
-    this.Total =  Number(this.Subtotal) + Number(this.SelectValue);
+  constructor(private cartService: CartService, private signal: CartWishingDataService, Shipping: EgyptShippingService, private sharedService: SharedServiceService) {
+    this.Shipping = Shipping.shipping;
+
+  }
+
+  ngOnInit(): void {
+
+    this.cartService.getCart().subscribe({
+      next: (data) => {
+        this.Cart = data,
+        this.Cart.items.forEach(x => {
+          this.Subtotal += x.price * x.quantity;
+        });
+        this.sharedService.SubTotal.next(this.Subtotal);
+        if (this.Cart.items.length == 0)
+          this.EmptyCart = false;
+      },
+      error: (error) => this.EmptyCart = false
+    });
+
+    this.Total = this.Subtotal;
+    console.log(this.Cart);
+    //this.signal.cartItemsCount.set(this.Cart.items.length);
+
+
+
+  }
+  UpdateTotals() {
+
+    this.Total = Number(this.Subtotal) + Number(this.SelectValue);
     console.log(this.Total);
-   }
-   ClearCart()
-   {
+  }
+  ClearCart() {
     this.Cart.items.forEach(element => {
-      this.cartService.removeFromCart(element.id).subscribe({next : (x)=> console.log(x)}) ;
+      this.cartService.removeFromCart(element.id).subscribe({ next: (x) => console.log(x) });
 
     });
-    this.Cart.items=[];
+    this.Cart.items = [];
     this.recalculateTotals();
     this.signal.cartItemsCount.set(this.signal.cartItems.length);
 
 
 
-   }
-   UpdateCart(quantity: number , item : CartItems )
-   {
-      
-      
-      if(quantity == 0 )
-      {
-        // call delete item from cart ...
-         this.cartService.removeFromCart(item.id).subscribe({next : (x)=> console.log(x)}) ;
-         this.Cart.items = this.Cart.items.filter(x => x.id != item.id);
-           
-         this.recalculateTotals();
+  }
+  UpdateCart(quantity: number, item: CartItems) {
 
-      }
-      else 
-      {
-        item.quantity = quantity;
-        this.cartService.UpdateCart(item).subscribe({
-          next: cart=>
-          {
-            const index = cart.items.findIndex(x => x.id === item.id);
-            if (index > -1) {
-              this.Cart.items[index].quantity = quantity;
-            }
-            this.recalculateTotals();
 
-          },
-          error: (err) => console.error(err)
+    if (quantity == 0) {
+      // call delete item from cart ...
+      this.cartService.removeFromCart(item.id).subscribe({ next: (x) => console.log(x) });
+      this.Cart.items = this.Cart.items.filter(x => x.id != item.id);
+
+      this.recalculateTotals();
+
+    }
+    else {
+      item.quantity = quantity;
+      this.cartService.UpdateCart(item).subscribe({
+        next: cart => {
+          const index = cart.items.findIndex(x => x.id === item.id);
+          if (index > -1) {
+            this.Cart.items[index].quantity = quantity;
+          }
+          this.recalculateTotals();
+
+        },
+        error: (err) => console.error(err)
       });
-      }
-      this.signal.cartItemsCount.set(this.Cart.items.length);
+    }
+    this.signal.cartItemsCount.set(this.Cart.items.length);
 
 
 
-   }
-   recalculateTotals() {
+  }
+  recalculateTotals() {
     this.Subtotal = 0;
     this.Cart.items.forEach(x => {
       this.Subtotal += x.price * x.quantity;
@@ -118,6 +113,6 @@ export class CartComponent implements OnInit {
     this.EmptyCart = !(this.Cart.items.length === 0);
     this.UpdateTotals();
   }
-  
-   
+
+
 }
